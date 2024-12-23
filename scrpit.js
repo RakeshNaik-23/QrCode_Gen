2868d28c25ad96f1e4181ef0c0ecf9b99f3c7a2f
RakeshNaik-23 <163093710+RakeshNaik-23@users.noreply.github.com>
2024-12-23 14:17:43 +0530

    Update scrpit.js

--- scrpit.js
+++ scrpit.js
@@ -1,12 +1,8 @@
-let form=document.querySelector("form")
-form.addEventListener("submit",(e)=>{
-    e.preventDefault()
-    let input=document.querySelector("#url").value
-    // console.dir(input);
-    let image=document.querySelector("img")
-    // console.dir(image);
-    let urlqr=`https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=${input}`   //* to generate QR CODE we requried one link i.e. QR CHART BY GOOGLE
-    image.src=urlqr;
-})
-
-
+let form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let input = document.querySelector("#url").value;
+    let image = document.querySelector("img");
+    let urlqr = `https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=${encodeURIComponent(input)}`;
+    image.src = urlqr;
+});
